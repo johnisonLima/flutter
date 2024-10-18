dc2014620d1cab66818e2146169f28eaa4dbdc27
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2024-10-18 11:39:02 -0300

    Segunda parte sobre Stateless

--- projects/dev/ola_mundo/lib/main.dart
+++ projects/dev/ola_mundo/lib/main.dart
@@ -1,36 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  const int valor = 10;
+
+  runApp(const App('Aplicativo olá, mundo!', valor));
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final String? title;
+  final int? valor;
+
+  const App(this.title, this.valor, {super.key});
+
+  // const App(String s, {Key? key, this.title, this.valor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Aplicativo olá, mundo!',
-            style: TextStyle(
-              color: Colors.white,
-            )
-          ),
-          backgroundColor: Colors.blue,
-        ),
-        body: const Center(
-          child: Text(
-            'Olá, mundo!',
-            style: TextStyle(
-              fontSize: 50,
-              color: Colors.cyan,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(title!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  )),
+              backgroundColor: Colors.blue,
             ),
-          )
-        )
-      )
-    );
+            body: Center(
+                child: Text(
+              'Olá, mundo!, valor é $valor',
+              style: const TextStyle(
+                fontSize: 50,
+                color: Colors.cyan,
+              ),
+            ))));
   }
 }
