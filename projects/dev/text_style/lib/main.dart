26dfb3c6ed8e0edf500c097254f8fe7d69439b13
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2024-10-24 21:39:30 -0300

    Alterando a fonte de todo aplicativo usando theme

--- projects/dev/text_style/lib/main.dart
+++ projects/dev/text_style/lib/main.dart
@@ -9,21 +9,26 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
+    return MaterialApp(
+
+      theme: ThemeData(
+        fontFamily: 'imagine',
+      ),
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Aplicativo fonte externa', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Center(
+          child: Text(
           'Texto 1\nTexto 2', 
           textDirection: TextDirection.ltr,
           style: TextStyle(
             fontSize: 50,
-            color: Colors.blue[200],
-            backgroundColor: Colors.yellow[50],
-            height: 1.50,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-            decoration: TextDecoration.lineThrough,
-            fontFamily: 'imagine',
+            color: Colors.blue[200],                      
           ),
+        ),
         ),
       ),
     );
