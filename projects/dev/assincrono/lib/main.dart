1d1803228c4e5bf090d45d14bb7035047c86d828
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2024-10-26 23:09:07 -0300

    Criando uma função Future

--- projects/dev/assincrono/lib/main.dart
+++ projects/dev/assincrono/lib/main.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
 
-void main() {
-  Future<void> quatro = Future.delayed(const Duration(seconds: 3));
-
+void main () async{
   print(1);
   print(2);
   print(3);
-  
-  quatro.then(
-    (value) => print(4),
-  );
+
+  await quatro();
 
   print(5);
+}
+
+Future<void> quatro() async {
+  return Future.delayed(
+    const Duration(seconds: 3),
+    () => print(4)
+  );
 }
