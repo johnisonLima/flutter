7557f855055ebf42d00398e77fc2ba2eccc294c5
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2024-10-27 22:21:37 -0300

    Usando Classe para tratamento de JSON

--- projects/dev/requisicao_http_json/lib/main.dart
+++ projects/dev/requisicao_http_json/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() async {
-  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/');
+  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
 
   final future = http.get(uri);
 
@@ -11,14 +11,44 @@ void main() async {
     if (response.statusCode == 200) {
       print('Página carregada.');
 
-      var list = jsonDecode(response.body) as List;
+      Map<String, dynamic> dados = jsonDecode(response.body);
 
-      list.forEach((elemento) {
-        print(elemento['id']);
-      });
+      Album album = Album.fromJson(dados);
 
+      print(album.title);
+      print(album.toJson());
     } else {
       print('Erro.');
     }
   });
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      completed: json['completed'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'userId': this.userId,
+    'id': this.id,
+    'title': this.title,
+    'completed': this.completed,
+  };
 }
