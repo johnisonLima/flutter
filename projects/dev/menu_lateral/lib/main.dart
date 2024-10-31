ab160cbd981fb90432a871f589d8045b5215ec44
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2024-10-31 13:08:51 -0300

    Navegação entre telas menu Drawer

--- projects/dev/menu_lateral/lib/main.dart
+++ projects/dev/menu_lateral/lib/main.dart
@@ -15,42 +15,112 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            endDrawer: Drawer(
-              child: Column(
-                children: [
-                  UserAccountsDrawerHeader(
-                    currentAccountPicture: Image.network('https://picsum.photos/50/50'),
+      home: Scaffold(
+        endDrawer: Builder(
+          builder: (context) => Drawer(
+            child: Column(
+              children: [
+                UserAccountsDrawerHeader(
+                    currentAccountPicture:
+                        Image.network('https://picsum.photos/50/50'),
                     accountName: const Text('Johnison'),
-                    accountEmail: const Text('email@email.com')
-                  ),
-                  const DrawerHeader(
-                    child: Text('Drawer Header'),
-                  ),
-                  ListTile(
+                    accountEmail: const Text('email@email.com')),
+                const DrawerHeader(
+                  child: Text('Drawer Header'),
+                ),
+                ListTile(
                     leading: const Icon(Icons.attach_money),
-                    title: const Text('Menu Lateral'),
+                    title: const Text('Tela 1'),
                     // subtitle: const Text('Selecione a opção no menu'),
                     onTap: () {
-                      print('Clicou dólar');
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.edit),
-                    title: const Text('Editar Cadastro'),
-                    onTap: () {
-                      print('Clicou editar');
-                    },
-                  ),
-                ],
-              ),
+                      Navigator.of(context).pop();
+
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                          return const Tela1();
+                        }),
+                      );
+                    }),
+                ListTile(
+                  leading: const Icon(Icons.edit),
+                  title: const Text('Tela 2'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) {
+                        return const Tela2();
+                      }),
+                    );
+                  },
+                ),
+              ],
             ),
-            appBar: AppBar(
-              title: const Center(child: Text('Menu Lateral(Drawer)')),
-              backgroundColor: Colors.blueGrey.shade200,
-            ),
-            body: const Center(
-              child: Text('Meu aplicativo'),
-            )));
+          ),
+        ),
+        appBar: AppBar(
+          title: const Center(child: Text('Menu Lateral(Drawer)')),
+          backgroundColor: Colors.blueGrey.shade200,
+        ),
+        body: const Center(
+          child: Text('Meu aplicativo'),
+        ),
+      ),
+    );
+  }
+}
+
+class Tela1 extends StatelessWidget {
+  const Tela1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Tela 1',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Voltar para aplicativo'),
+        ),
+      ),
+    ));
+  }
+}
+
+class Tela2 extends StatelessWidget {
+  const Tela2({super.key});
+
+  static const rountName = '/tela2';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Tela 2',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Voltar para aplicativo'),
+          ),
+        ),
+      ),
+    );
   }
 }
